b1a68507581f30ae485ab354e107168f1ad7ae93
sudarvds <vdsudar@gmail.com>
2026-02-27 11:19:58 +0400

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI(title="AI Resume Category Predictor")
+
+model = joblib.load("resume_model.pkl")
+
+class ResumeInput(BaseModel):
+    resume_text: str
+
+@app.get("/")
+def home():
+    return {"message": "Resume Prediction API Running"}
+
+@app.post("/predict")
+def predict(data: ResumeInput):
+    prediction = model.predict([data.resume_text])[0]
+    probability = max(model.predict_proba([data.resume_text])[0])
+
+    return {
+        "predicted_category": prediction,
+        "confidence_score": round(float(probability), 3)
+    }
+
+!uvicorn app:app --host 0.0.0.0 --port 10000
