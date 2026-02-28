332727c3080c396fe70883ccf238fe91b07e8a4d
sudarvds <vdsudar@gmail.com>
2026-02-28 09:31:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,6 @@ async def predict(resume: UploadFile = File(...)):
     return {
         "predicted_category": prediction
     }
-uvicorn app:app --host 0.0.0.0 --port 10000
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=10000)
